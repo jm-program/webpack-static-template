ddc12e85e34e49eaefcfcf4dd44d6a2986dbecb6
Alex Karo <alex@karo-dev.ru>
2019-08-20 13:04:35 +0300

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+Based on [this article](https://hackernoon.com/lets-start-with-webpack-4-91a0f1dba02e)
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  // Входной файл
+  entry: [
+    './src/js/index.js'
+  ],
+
+  // Выходной файл
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map", // Source maps для удобства отладки
+
+  module: {
+    rules: [
+      // Транспилируем js с babel
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+
+      // Компилируем SCSS в CSS
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extract css to separate file
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+
+      // Подключаем шрифты из css
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./fonts/[name].[ext]'
+          },
+        ]
+      },
+    ],
+  },
+  plugins: [
+    // Подключаем файл html, стили и скрипты встроятся автоматически
+    new HtmlWebpackPlugin({
+      title: 'Webpack 4 Starter',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      }
+    }),
+
+    // Кладем стили в отдельный файлик
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+
+    // Подключаем автогенерацию вендорных префиксов
+    require('autoprefixer'),
+
+    // Копируем картинки
+    new CopyWebpackPlugin([
+      {
+        from: './src/img',
+        to: 'img',
+      },
+    ])
+  ],
+};
